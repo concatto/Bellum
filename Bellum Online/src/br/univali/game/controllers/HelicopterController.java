41a77d5654e6f0c7723228df3af1e4b82631d26d
ViniciusAS <vinicius@rudinei.cnt.br>
2016-11-30 21:32:07 -0200

    Player Controller alterado para abstrato

---
+++ Bellum Online/src/br/univali/game/controllers/HelicopterController.java
@@ -0,0 +1,17 @@
+package br.univali.game.controllers;
+
+import br.univali.game.objects.GameObjectCollection;
+
+public class HelicopterController extends PlayerController {
+
+	public HelicopterController(GameObjectCollection collection) {
+		super(collection);
+		
+	}
+
+	@Override
+	public void update(float delta) {
+		
+	}
+
+}
